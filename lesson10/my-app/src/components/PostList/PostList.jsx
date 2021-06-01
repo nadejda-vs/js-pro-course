bee55be16e3098ed230feb2941013755c108359b
Nadya Yakatsuk <nadejdagrand27@gmail.com>
2021-06-01 15:55:54 +0300

    add task

--- lesson10/my-app/src/components/PostList/PostList.jsx
+++ lesson10/my-app/src/components/PostList/PostList.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState, memo } from 'react';
 import { Post } from '../Post';
+
 export const PostList = memo(() => {
-	const [users, setUsers] = useState([]);
 	const [posts, setPosts] = useState([]);
+
 	useEffect(async () => {
 		const response = await fetch('https://jsonplaceholder.typicode.com/users');
 		const usersResponse = await response.json();
@@ -10,18 +11,20 @@ export const PostList = memo(() => {
 			'https://jsonplaceholder.typicode.com/posts'
 		);
 		const postsResponse = await responsePost.json();
-		setUsers(usersResponse);
-		setPosts(postsResponse);
+
+		const posts = postsResponse.map((post) => {
+			const user = usersResponse.find((user) => user.id === post.userId);
+
+			return { ...post, user };
+		});
+
+		setPosts(posts);
 	}, []);
+
 	return (
 		<ol>
-			{users.map((item) => (
-				<li>{item.name}</li>
-				// <Post username={item.username} />
-			))}
 			{posts.map((item) => (
-				// <Post title={item.title} body={item.body} />
-				<li>{item.body}</li>
+				<Post username={item.username} title={item.title} body={item.body} />
 			))}
 		</ol>
 	);
