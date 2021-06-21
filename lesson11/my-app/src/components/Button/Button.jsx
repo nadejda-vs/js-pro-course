ed07ca6c2870b8499c9a5ef7e5a9865873244e91
nadejda-vs <75483713+nadejda-vs@users.noreply.github.com>
2021-06-21 18:51:21 +0300

    Delete lesson11/my-app directory

--- lesson11/my-app/src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-export const Button = (props) => {
-	return (
-		<div className="button">
-			<button>{props.buttonName}</button>
-		</div>
-	);
-};
--- lesson11/my-app/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Post';
-export * from './Facebook';
-export * from './Button';
